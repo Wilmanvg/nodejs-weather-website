0b1ba24aff67fd317cfcae55c5eafea3ce40a654
Wilman Garcia <wvgarcia@usc.edu>
2020-01-05 07:39:40 -0800

    Added more data to the weather forecast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' +
-             body.currently.precipProbability + '% chance of rain')
+             body.currently.precipProbability + '% chance of rain. The high for the day is ' + body.daily.data[0].temperatureHigh + ' and the low is ' + body.daily.data[0].temperatureLow)
         }
     })
 }
